edcea17c61c80377dbcf2960df62dd4310deb7d1
Nelson Susanto <nelson.hai.97@gmail.com>
2022-02-11 13:23:24 +1100

    Minor fixes

--- octo-take-home/Program.fsx
+++ octo-take-home/Program.fsx
@@ -9,6 +9,7 @@ let getReleasesForProject p =
     let releases = File.ReadAllText "Data/Releases.json" |> Json.deserialize<Release list>
     List.filter (isInProject p) releases
 
+
 let getLatestDeploymentForEnvAndRelease (e:Environment) (r:Release) =
     let deployments = File.ReadAllText "Data/Deployments.json" |> Json.deserialize<Deployment list>
     let deploymentsForEnvAndRelease = List.filter (fun d->d.EnvironmentId = e.Id && d.ReleaseId = r.Id) deployments
@@ -17,9 +18,11 @@ let getLatestDeploymentForEnvAndRelease (e:Environment) (r:Release) =
     | [d] -> Some d
     | _ -> Some(List.reduce (fun curr latest -> if curr.DeployedAt > latest.DeployedAt then curr else latest) deploymentsForEnvAndRelease)
 
+
 let logReasonForRetention list =
     List.map (fun x->printfn $"{x.ReleaseId} kept because it was the most recently deployed to {x.EnvironmentId}") list |> ignore
     list
+
 
 let keepNLatestReleases n (envAndReleases:EnvironmentAndReleases) =
     let env = envAndReleases |> fst
@@ -32,6 +35,7 @@ let keepNLatestReleases n (envAndReleases:EnvironmentAndReleases) =
     |> logReasonForRetention
     |> List.map (fun x->x.ReleaseId)
 
+
 let releaseRetention n =
     let projects = File.ReadAllText "Data/Projects.json" |> Json.deserialize<Project list>
     let environments = File.ReadAllText "Data/Environments.json" |> Json.deserialize<Environment list>
@@ -39,5 +43,5 @@ let releaseRetention n =
         |> List.allPairs environments
         |> List.map(keepNLatestReleases(n))
 
-let result = releaseRetention 1
-0
+
+releaseRetention 1
